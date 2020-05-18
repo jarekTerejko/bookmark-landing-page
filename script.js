01d154720c5d0078d10f5c9d77609862c06edcc2
Jarosław Terejko <j.terejko@gmail.com>
2020-05-18 06:48:33 +0200

    added features section

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
-console.log(1)
+console.log(1);
 
-const questionCont = document.querySelectorAll('.faq__question-container')
+const questionCont = document.querySelectorAll(".faq__question-container");
 
-const answerContainer = document.querySelectorAll('.faq__answer-container')
+const answerContainer = document.querySelectorAll(".faq__answer-container");
 
-answerContainer.forEach(answer => {
-    answer.classList.add("inactive")
-})
+answerContainer.forEach((answer) => {
+  answer.classList.add("inactive");
+});
 
-questionCont.forEach(question => {
-    question.addEventListener("click", ()=> {
-        const angleArrow = question.lastElementChild
-        const answer = question.nextElementSibling
-        if(answer.classList.contains('inactive')){
-            answer.classList.remove('inactive')
-            answer.classList.add('active')
-            angleArrow.classList.add('rotated')
-        } else {
-            answer.classList.remove('active')
-            answer.classList.add('inactive')
-            angleArrow.classList.remove('rotated')
-        }
+questionCont.forEach((question) => {
+  question.addEventListener("click", () => {
+    const angleArrow = question.lastElementChild;
+    const answer = question.nextElementSibling;
+    if (answer.classList.contains("inactive")) {
+      answer.classList.remove("inactive");
+      answer.classList.add("active");
+      angleArrow.classList.add("rotated");
+    } else {
+      answer.classList.remove("active");
+      answer.classList.add("inactive");
+      angleArrow.classList.remove("rotated");
+    }
+  });
+});
 
-    })
-})
+const tabBtns = document.querySelectorAll(".features__btn");
+
+const tabs = document.querySelectorAll(".features__tab");
+
+tabBtns.forEach((tabBtn) => {
+  tabBtn.addEventListener("click", () => {
+    const target = document.querySelector(tabBtn.dataset.tabTarget);
+
+    if (tabBtn.classList.contains("tab-active")) return;
+
+    tabBtns.forEach((tabBtn) => {
+      tabBtn.classList.remove("tab-active");
+    });
+    tabBtn.classList.add("tab-active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-content");
+    });
+
+    target.classList.add("active-content");
+  });
+});
