38ef62e346a4df1ff2111ab064d47e0fb03bd3ec
Jarosław Terejko <j.terejko@gmail.com>
2020-05-20 06:00:06 +0200

    added contact section

--- script.js
+++ script.js
@@ -46,3 +46,51 @@ tabBtns.forEach((tabBtn) => {
     target.classList.add("active-content");
   });
 });
+
+const form = document.querySelector(".contact__form");
+const email = document.querySelector(".contact__email-input");
+const circleError = document.querySelector(".fa-exclamation-circle");
+const msgContainer = document.querySelector(".contact__info-msg");
+
+const isMailValid = (email) => {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+};
+
+const checkEmail = () => {
+  const emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    setError(email, circleError, "Email cannot be blank...");
+  } else if (!isMailValid(emailValue)) {
+    setError(email, circleError, "Whoops, make sure it's an email");
+  } else {
+    setSuccess(email, circleError, "Thank you!");
+  }
+};
+
+const setError = (input, circleErr, msg) => {
+  input.className = "contact__email-input contact__email-input--error";
+  circleErr.className = "fas fa-exclamation-circle";
+  msgContainer.className = "contact__info-msg contact__info-msg--visible-error";
+  msgContainer.textContent = msg;
+};
+
+const setSuccess = (input, circleErr, msg) => {
+  input.className = "contact__email-input";
+  circleErr.className =
+    "fas fa-exclamation-circle fa-exclamation-circle--error";
+  msgContainer.className =
+    "contact__info-msg contact__info-msg--visible-success";
+  msgContainer.textContent = msg;
+};
+
+form.addEventListener("submit", (e) => {
+  checkEmail();
+  // to prevent site from reload event if input is correct
+  e.preventDefault();
+  if (email.classList.contains("contact__email-input--error")) {
+    e.preventDefault();
+  }
+});
